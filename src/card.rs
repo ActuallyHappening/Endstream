9191c494225fa8c713241535a340af3eaf73bf3b
ActuallyHappening <105958073+ActuallyHappening@users.noreply.github.com>
2023-07-03 15:57:08 +1000

    add: numbers

--- src/card.rs
+++ src/card.rs
@@ -202,16 +202,18 @@ fn construct_card_from_visual(
 					cost_frame.name("Cost frame");
 					// end cost frame
 
-					// agenda icons
+					// agenda icons & numbers
 					cost_frame.with_children(|cost_frame| {
 						let mesh = shape::Quad::new(Vec2::new(AgendaType::width, AgendaType::height));
 						let mesh = meshs.add(mesh.into());
+						const number_size: f32 = 0.2;
+
 						match &agenda_cost {
 							AgendaCost::One { only } => {
+								// icon
 								let material = texture_2d(ass.load(only.agenda.get_icon_asset_path()));
 								// -0.05 is a slight offset, magic number
-								let transform =
-									Transform::from_xyz(0.0 - 0.05, -0.05, almost_zero);
+								let transform = Transform::from_xyz(0.0 - 0.05, -0.05, almost_zero);
 
 								cost_frame
 									.spawn(PbrBundle {
@@ -221,6 +223,22 @@ fn construct_card_from_visual(
 										..default()
 									})
 									.name("Only agenda");
+
+								// number
+								let (mesh, offset) = get_text_mesh(&only.number.to_string(), number_size);
+								let top_right_transform = Transform::from_xyz(
+									cost_frame_shape.size.x / 2. - number_size / 2.,
+									cost_frame_shape.size.y / 2. - number_size / 2.,
+									almost_zero,
+								);
+								cost_frame
+									.spawn(PbrBundle {
+										transform: top_right_transform.translate(offset),
+										mesh: meshs.add(mesh),
+										material: mat.add(Color::WHITE.into()),
+										..default()
+									})
+									.name("Number for only agenda");
 							}
 							AgendaCost::Two { first, second } => {
 								// first
@@ -238,6 +256,22 @@ fn construct_card_from_visual(
 									})
 									.name("First agenda");
 
+								// first number
+								let (t_mesh, offset) = get_text_mesh(&second.number.to_string(), number_size);
+								let top_right_transform = Transform::from_xyz(
+									- number_size / 2.,
+									cost_frame_shape.size.y / 2. - number_size / 2.,
+									almost_zero,
+								);
+								cost_frame
+									.spawn(PbrBundle {
+										transform: top_right_transform.translate(offset),
+										mesh: meshs.add(t_mesh),
+										material: mat.add(Color::WHITE.into()),
+										..default()
+									})
+									.name("Number for only agenda");
+
 								// second
 								let material = texture_2d(ass.load(second.agenda.get_icon_asset_path()));
 								// -0.05 is a slight offset, magic number
@@ -252,10 +286,26 @@ fn construct_card_from_visual(
 										..default()
 									})
 									.name("Second agenda");
+
+								// second number
+								let (mesh, offset) = get_text_mesh(&second.number.to_string(), number_size);
+								let top_right_transform = Transform::from_xyz(
+									cost_frame_shape.size.x / 2. - number_size / 2.,
+									cost_frame_shape.size.y / 2. - number_size / 2.,
+									almost_zero,
+								);
+								cost_frame
+									.spawn(PbrBundle {
+										transform: top_right_transform.translate(offset),
+										mesh: meshs.add(mesh),
+										material: mat.add(Color::WHITE.into()),
+										..default()
+									})
+									.name("Number for only agenda");
 							}
 						}
 					});
-					// end icons
+					// end icons & numbers
 				});
 		}
 		// end agenda cost
@@ -270,14 +320,14 @@ pub fn spawn_all_cards_debug(mut commands: Commands, mut ass: ASS) {
 		CardVisual {
 			bg: CardVisualBg::Blackish,
 			start_century: Some(Century::S2100),
-			// activation_cost: Some(AgendaCost::new_double(
-			// 	(
-			// 		2,
-			// 		(SingleAgendaType::Military, SingleAgendaType::Wild).into(),
-			// 	),
-			// 	(3, SingleAgendaType::Science.into()),
-			// )),
-			activation_cost: Some(AgendaCost::new_single(3, SingleAgendaType::Politics.into())),
+			activation_cost: Some(AgendaCost::new_double(
+				(
+					2,
+					(SingleAgendaType::Military, SingleAgendaType::Wild).into(),
+				),
+				(3, SingleAgendaType::Science.into()),
+			)),
+			// activation_cost: Some(AgendaCost::new_single(3, SingleAgendaType::Politics.into())),
 		},
 		Transform::from_xyz(CARD_WIDTH + 2., 5.0, 0.),
 		&mut commands,
