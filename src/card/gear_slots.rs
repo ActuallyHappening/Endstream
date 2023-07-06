51c0b488a2f2edbb0d0c44cdeac7c42fc171ee6e
ActuallyHappening <105958073+ActuallyHappening@users.noreply.github.com>
2023-07-06 11:41:07 +1000

    add: gear frames

--- src/card/gear_slots.rs
+++ src/card/gear_slots.rs
@@ -3,8 +3,9 @@ use derive_more::Display;
 use strum::EnumIs;
 
 use crate::{
+	card::{right_margin, CARD_WIDTH},
 	ext::{EntityCommandsExt, IntoAssetPath, SpawnToParent, TransformExt},
-	texture_2d, card::{CARD_WIDTH, right_margin},
+	texture_2d,
 };
 
 use super::almost_zero;
@@ -22,6 +23,8 @@ pub struct GearSlots {
 	pub first: GearType,
 	pub second: GearType,
 }
+
+struct GearSlotFrame;
 
 impl GearSlots {
 	pub fn new(first: GearType, second: GearType) -> GearSlots {
@@ -43,8 +46,8 @@ impl IntoAssetPath for GearType {
 }
 
 impl GearType {
-	const width: f32 = 0.35;
-	const height: f32 = 0.41;
+	const width: f32 = 0.25;
+	const height: f32 = 0.3;
 }
 
 impl SpawnToParent for GearType {
@@ -65,8 +68,39 @@ impl SpawnToParent for GearType {
 	}
 }
 
+impl IntoAssetPath for GearSlotFrame {
+	fn get_asset_path(&self) -> String {
+		"card-icons/gear-frame.png".to_string()
+	}
+}
+
+impl GearSlotFrame {
+	const icon_padding: f32 = 0.05;
+	const width: f32 = GearType::width + GearSlotFrame::icon_padding * 2.;
+	const height: f32 = GearType::height + GearSlotFrame::icon_padding * 2.;
+}
+
+impl SpawnToParent for GearSlotFrame {
+	fn spawn_using_entity_commands(
+		&self,
+		parent: &mut ChildBuilder<'_, '_, '_>,
+		translation: Vec3,
+		(meshs, mat, ass): crate::ext::mutASS,
+	) -> Entity {
+		let mut frame = parent.spawn(PbrBundle {
+			transform: Transform::from_translation(translation),
+			mesh: meshs
+				.add(shape::Quad::new(Vec2::new(GearSlotFrame::width, GearSlotFrame::height)).into()),
+			material: mat.add(texture_2d(ass.load(self.get_asset_path()))),
+			..default()
+		});
+		frame.name("GearSlotFrame");
+		frame.id()
+	}
+}
+
 impl GearSlots {
-	const margin: f32 = 0.1;
+	const margin: f32 = 0.15;
 	pub const width: f32 = GearType::width * 2.0 + GearSlots::margin;
 }
 
@@ -86,14 +120,22 @@ impl SpawnToParent for GearSlots {
 		/* #region second */
 		const right_x: f32 = GearSlots::margin / 2. + GearType::width / 2.;
 		parent.with_children(|parent| {
-			self.second.spawn_using_entity_commands(parent, Vec3::X * right_x, (meshs, mat, ass));
+			let translation = Vec3::X * right_x;
+			self
+				.second
+				.spawn_using_entity_commands(parent, translation, (meshs, mat, ass));
+			GearSlotFrame.spawn_using_entity_commands(parent, translation, (meshs, mat, ass));
 		});
 		/* #endregion */
 
 		/* #region first */
 		const left_z: f32 = -right_x;
 		parent.with_children(|parent| {
-			self.first.spawn_using_entity_commands(parent, Vec3::X * left_z, (meshs, mat, ass));
+			let translation = Vec3::X * left_z;
+			self
+				.first
+				.spawn_using_entity_commands(parent, translation, (meshs, mat, ass));
+			GearSlotFrame.spawn_using_entity_commands(parent, translation, (meshs, mat, ass));
 		});
 		/* #endregion */
 
